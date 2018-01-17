175d0256e22cd4c041f6fb1a57a85e43845db6d4
ewarrior77 <35514576+ewarrior77@users.noreply.github.com>
2018-01-17 11:37:56 +0800

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+import "net"
+import "bufio"
+
+func main() {
+    fmt.Printf("Hello, world\n")
+
+    conn, err := net.Dial("tcp", "golang.org:80")
+    if err != nil {
+        fmt.Printf("Connection error!\n")
+    } else {
+	    fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	    status, err := bufio.NewReader(conn).ReadString('\n')
+	    if err != nil {
+	    	fmt.Printf("Read error!\n")
+	    } else {
+	    	fmt.Printf("Connection OK \n%s", status)
+	    }
+	}
+}
